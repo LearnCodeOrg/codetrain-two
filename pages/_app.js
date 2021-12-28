60ccbaf8a7e665ae957df5d5e8c0f2450ba85a23
Cooper Saye <coopersaye@gmail.com>
2021-12-27 23:32:33 -0800

    Initialize firebase.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import Head from 'next/head';
 
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from '../util/firebaseConfig';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import '../styles/globals.css';
+
+// initialize firebase
+if (!getApps().length) initializeApp(firebaseConfig);
 
 export default function App(props) {
   const { Component, pageProps } = props;
+
+  // listen for auth
+  const auth = getAuth()
+  useAuthState(auth);
 
   return (
     <>
---
+++ util/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDu_VbW1bzdLlY_aFsQnVQ_TXtczrW1unc",
+  authDomain: "app.codetrain.org",
+  projectId: "codetrain-org",
+  storageBucket: "codetrain-org.appspot.com",
+  messagingSenderId: "755030431905",
+  appId: "1:755030431905:web:2eeb48ccd6e985a338f8e0"
+};
