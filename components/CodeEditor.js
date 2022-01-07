6dc15444adde64d93e81a15cb636dfc1e654b48d
Cooper Saye <coopersaye@gmail.com>
2022-01-06 23:48:56 -0800

    Create CodeEditor component.

---
+++ components/CodeEditor.js
@@ -0,0 +1,25 @@
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-javascript.js';
+import 'ace-builds/src-noconflict/theme-monokai.js';
+import 'ace-builds/src-noconflict/ext-language_tools';
+import 'ace-builds/src-noconflict/ext-searchbox';
+
+export default function CodeEditor(props) {
+  return (
+    <AceEditor
+      mode="javascript"
+      theme="monokai"
+      wrapEnabled={true}
+      showPrintMargin={false}
+      tabSize={2}
+      setOptions={{
+        useWorker: false,
+        enableLiveAutocompletion: true
+      }}
+      width="500px"
+      height="500px"
+      {...props}
+    />
+  );
+}
