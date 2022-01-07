958baaab409a6599a26f5309ee3cc9e81b740b7e
Cooper Saye <coopersaye@gmail.com>
2022-01-06 23:26:37 -0800

    Clean up _app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Main from '../components/Main';
 import Head from 'next/head';
 
 import { initializeApp, getApps } from 'firebase/app';
@@ -11,8 +12,6 @@ import '../styles/globals.css';
 if (!getApps().length) initializeApp(firebaseConfig);
 
 export default function App(props) {
-  const { Component, pageProps } = props;
-
   // listen for auth
   const auth = getAuth()
   useAuthState(auth);
@@ -22,9 +21,11 @@ export default function App(props) {
       <Head>
         <title>Codetrain</title>
         <meta name="description" content="A retro game engine in the browser." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
       </Head>
-      <Component {...pageProps} />
+      <Main {...props} />
     </>
   );
 }
