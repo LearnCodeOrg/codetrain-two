e18d1815baf0915c8e05103296f1e0fef3f36ba3
Cooper Saye <coopersaye@gmail.com>
2022-01-09 23:36:20 -0800

    Handle current object state.

--- components/Objects.js
+++ components/Objects.js
@@ -1,12 +1,20 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import styles from '../styles/components/Objects.module.css';
 
 let canvas;
 let ctx;
 
+const gridHeight = 4;
+const gridWidth = 4;
+const gridPixels = 16;
+const canvasWidth = gridWidth * gridPixels;
+const canvasHeight = gridHeight * gridPixels;
+
 export default function Objects() {
   const canvasRef = useRef();
+
+  const [currObject, setCurrObject] = useState(0);
 
   // get canvas context on start
   useEffect(() => {
@@ -19,6 +27,9 @@ export default function Objects() {
       <h1>Objects</h1>
       <canvas
         ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+        onMouseDown={select}
       />
     </div>
   );
