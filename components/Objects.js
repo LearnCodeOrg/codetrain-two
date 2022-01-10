9380680c19c9840cbe5f191c8a84fb76f9fd03e5
Cooper Saye <coopersaye@gmail.com>
2022-01-09 23:28:49 -0800

    Initialize objects canvas.

--- components/Objects.js
+++ components/Objects.js
@@ -1,8 +1,25 @@
+import { useEffect, useRef } from 'react';
+
 import styles from '../styles/components/Objects.module.css';
 
+let canvas;
+let ctx;
+
 export default function Objects() {
+  const canvasRef = useRef();
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <h1>Objects</h1>
+      <canvas
+        ref={canvasRef}
+      />
     </div>
   );
 }
