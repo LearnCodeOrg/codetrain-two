455ada568207e7b0ace317de2a93ae2f04c09367
Cooper Saye <coopersaye@gmail.com>
2022-01-12 11:34:42 -0800

    Implement interactive logo.

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { useState } from 'react';
+
 import styles from '../styles/components/Header.module.css';
 
+const logo = '/img/logo.png';
+const logoalt = '/img/logoalt.png';
+
 export default function Header() {
+  const [logoSrc, setLogoSrc] = useState(logo);
+
   return (
     <div className={styles.container}>
       <Link href="/">
         <a>
-          <Image
-            src="/img/logo.png"
+          <img
+            src={logoSrc}
+            onMouseEnter={() => setLogoSrc(logoalt)}
+            onMouseLeave={() => setLogoSrc(logo)}
             width="48"
             height="48"
-            quality="100"
           />
         </a>
       </Link>
