84ee0f81ec09cd3b341c9b3228c90bd2c1371b42
Cooper Saye <coopersaye@gmail.com>
2022-01-12 23:00:39 -0800

    Initialize game iframe.

--- components/GameFrame.js
+++ components/GameFrame.js
@@ -1,8 +1,19 @@
-import styles from '../styles/components/GameFrame.module.css';
+import { useEffect, useRef, useState } from 'react';
 
 export default function GameFrame() {
+  const screenRef = useRef();
+
   return (
     <div>
+      <iframe
+        ref={screenRef}
+        title="game"
+        sandbox="allow-scripts"
+        srcDoc={source}
+        width={mapPixels}
+        height={mapPixels}
+        frameBorder="0"
+      />
     </div>
   );
 }
