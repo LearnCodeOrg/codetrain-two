d24ed725b07ddac1ac1995be2fbc32dc81310717
Cooper Saye <coopersaye@gmail.com>
2022-01-12 22:56:51 -0800

    Populate create page.

--- pages/create.js
+++ pages/create.js
@@ -1,8 +1,21 @@
+import Header from '../components/Header';
+import Objects from '../components/Objects';
+import Loading from '../components/Loading';
+
+import dynamic from 'next/dynamic';
+
 import styles from '../styles/pages/Create.module.css';
+
+const CodeEditor = dynamic(import('../components/CodeEditor'), {
+  loading: function Load() { return <Loading /> }, ssr: false
+});
 
 export default function Create() {
   return (
     <div>
+      <Header />
+      <CodeEditor />
+      <Objects />
     </div>
   );
 }
