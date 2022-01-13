28be829bc6ce485626cad26eab0234dd720edcdf
Cooper Saye <coopersaye@gmail.com>
2022-01-12 23:01:32 -0800

    Focus frame on start.

--- components/GameFrame.js
+++ components/GameFrame.js
@@ -1,7 +1,22 @@
+import ReplayIcon from '@mui/icons-material/Replay';
+
 import { useEffect, useRef, useState } from 'react';
+import getGameSrc from '../util/getGameSrc';
+
+const mapPixels = 256;
 
 export default function GameFrame() {
   const screenRef = useRef();
+
+  // focuses on screen
+  function focus() {
+    screenRef.current.focus();
+  }
+
+  // focus frame on start
+  useEffect(() => {
+    focus();
+  }, []);
 
   return (
     <div>
