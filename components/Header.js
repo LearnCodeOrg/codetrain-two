2e3f7f1efd02e91fcd70afc70d6b26e253edbb69
Cooper Saye <coopersaye@gmail.com>
2022-01-24 08:01:05 -0800

    Clean up header.

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,21 @@
-import Image from 'next/image';
+import Logo from './Logo';
 import Link from 'next/link';
-
-import { useState } from 'react';
 
 import styles from '../styles/components/Header.module.css';
 
-const logo = '/img/logo.png';
-const logoalt = '/img/logoalt.png';
-
 export default function Header() {
-  const [logoSrc, setLogoSrc] = useState(logo);
-
   return (
     <div className={styles.container}>
       <Link href="/">
         <a>
-          <img
-            src={logoSrc}
-            onMouseEnter={() => setLogoSrc(logoalt)}
-            onMouseLeave={() => setLogoSrc(logo)}
-            width="48"
-            height="48"
-          />
+          <Logo />
         </a>
       </Link>
       <h1>Codetrain</h1>
       <span className="flexfill" />
+      <Link href="/">
+        <a className={styles.link}>Home</a>
+      </Link>
       <Link href="/create">
         <a className={styles.link}>Create</a>
       </Link>
