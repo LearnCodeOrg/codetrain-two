bb0003fa7e0042b5d5acb0204afec234fed82980
Cooper Saye <coopersaye@gmail.com>
2022-01-29 15:57:21 -0800

    Extract engine from create page.

--- components/Engine.js
+++ components/Engine.js
@@ -1,8 +1,19 @@
-import styles from '../components/Engine.module.css';
+import Objects from '../components/Objects';
+import Loading from '../components/Loading';
+
+import dynamic from 'next/dynamic';
+
+const CodeEditor = dynamic(import('../components/CodeEditor'), {
+  loading: function Load() { return <Loading /> }, ssr: false
+});
+
+import styles from '../styles/components/Engine.module.css';
 
 export default function Engine() {
   return (
     <div>
+      <CodeEditor />
+      <Objects />
     </div>
   );
 }
--- pages/create.js
+++ pages/create.js
@@ -1,21 +1,13 @@
 import Header from '../components/Header';
-import Objects from '../components/Objects';
-import Loading from '../components/Loading';
-
-import dynamic from 'next/dynamic';
+import Engine from '../components/Engine';
 
 import styles from '../styles/pages/Create.module.css';
-
-const CodeEditor = dynamic(import('../components/CodeEditor'), {
-  loading: function Load() { return <Loading /> }, ssr: false
-});
 
 export default function Create() {
   return (
     <div>
       <Header />
-      <CodeEditor />
-      <Objects />
+      <Engine />
     </div>
   );
 }
