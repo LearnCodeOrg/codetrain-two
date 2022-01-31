d158915ee4c974cb4cb79a1ec8773e9bb6e77437
Cooper Saye <coopersaye@gmail.com>
2022-01-31 15:42:35 -0800

    Create colors canvas.

--- components/Colors.js
+++ components/Colors.js
@@ -1,11 +1,21 @@
 import styles from '../styles/components/Colors.module.css';
 
+const tileCount = 4;
+const tilePx = 32;
+const canvasWidth = tileCount * tilePx;
+const canvasHeight = tilePx;
+
 export default function Colors(props) {
   const { colors, setColors, setCurrColor } = props;
 
   return (
     <div>
       <h1>Colors</h1>
+      <canvas
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+      />
       <div className={styles.tiles}>
         {
           colors.map((color, i) =>
