fa1bace1a129b06bb9e57898e21d51a0c596c25b
Cooper Saye <coopersaye@gmail.com>
2022-01-31 08:08:17 -0800

    Initialize draw canvas.

--- components/Draw.js
+++ components/Draw.js
@@ -1,6 +1,19 @@
+import { useRef } from 'react';
+
 import styles from '../styles/components/Draw.module.css';
 
+let sketching = false;
+
+let lastX, lastY;
+let canvas, ctx;
+
+const pixelPx = 16;
+const spriteSize = 4;
+const spritePx = spriteSize * pixelPx;
+
 export default function Draw() {
+  const canvasRef = useRef();
+
   function mouseDown(e) {
     lastX = undefined;
     lastY = undefined;
@@ -23,10 +36,13 @@ export default function Draw() {
   return (
     <div>
       <canvas
+        ref={canvasRef}
         onMouseDown={mouseDown}
         onMouseMove={mouseMove}
         onMouseUp={mouseUp}
         onMouseLeave={mouseLeave}
+        width={spritePx}
+        height={spritePx}
       />
     </div>
   );
