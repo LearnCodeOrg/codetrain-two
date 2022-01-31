196b14370d069cbcc2bb5a6e2c96221318ec9def
Cooper Saye <coopersaye@gmail.com>
2022-01-30 17:09:56 -0800

    Create default object functions.

--- util/defaultData.js
+++ util/defaultData.js
@@ -1,5 +1,18 @@
 export const objectCount = 16;
 
 export const defaultCodes = Array(objectCount).fill(0).map((val, i) => (
-  `// Object ${i}`
+`// Object ${i}
+class Object${i} extends GameObject() {
+  constructor() {
+    // runs when object constructed
+    super();
+  }
+  start() {
+    // runs after all objects constructed
+  }
+  update() {
+    // runs once every frame
+  }
+}
+`
 ));
