69931c51da580a9774d8bb08f8ee56ba566e5839
Cooper Saye <coopersaye@gmail.com>
2022-01-31 08:05:21 -0800

    Handle mouse on draw canvas.

--- components/Draw.js
+++ components/Draw.js
@@ -1,8 +1,33 @@
 import styles from '../styles/components/Draw.module.css';
 
 export default function Draw() {
+  function mouseDown(e) {
+    lastX = undefined;
+    lastY = undefined;
+    sketching = true;
+    sketch(e);
+  }
+
+  function mouseMove(e) {
+    if (sketching) sketch(e);
+  }
+
+  function mouseUp(e) {
+    sketching = false;
+  }
+
+  function mouseLeave(e) {
+    sketching = false;
+  }
+
   return (
     <div>
+      <canvas
+        onMouseDown={mouseDown}
+        onMouseMove={mouseMove}
+        onMouseUp={mouseUp}
+        onMouseLeave={mouseLeave}
+      />
     </div>
   );
 }
