0eb8730be15333f17af7f7aad343879a94de9842
Cooper Saye <coopersaye@gmail.com>
2022-01-30 16:38:46 -0800

    Handle code and object states.

--- components/Engine.js
+++ components/Engine.js
@@ -1,6 +1,7 @@
 import Objects from '../components/Objects';
 import Loading from '../components/Loading';
 
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
 
 const CodeEditor = dynamic(import('../components/CodeEditor'), {
@@ -9,11 +10,26 @@ const CodeEditor = dynamic(import('../components/CodeEditor'), {
 
 import styles from '../styles/components/Engine.module.css';
 
+const objectCount = 16;
+const defaultCodes = Array(objectCount).fill(0).map(
+  (val, i) => `// Object ${i}`
+);
+
 export default function Engine() {
+  const [currObject, setCurrObject] = useState(0);
+  const [codes, setCodes] = useState(defaultCodes);
+
   return (
     <div>
-      <CodeEditor />
-      <Objects />
+      <CodeEditor
+        codes={codes}
+        setCodes={setCodes}
+        currObject={currObject}
+      />
+      <Objects
+        currObject={currObject}
+        setCurrObject={setCurrObject}
+      />
     </div>
   );
 }
