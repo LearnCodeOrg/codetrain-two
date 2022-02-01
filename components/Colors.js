7d7ab0326d2988c4ce9d24a16cf28c7b9d3d6cfb
Cooper Saye <coopersaye@gmail.com>
2022-01-31 20:48:49 -0800

    Create color fill functions.

--- components/Colors.js
+++ components/Colors.js
@@ -14,6 +14,28 @@ export default function Colors(props) {
 
   const canvasRef = useRef();
 
+  // draw hover selector
+  function fillHover(x, y, width, height) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(x, y, border * 2, border);
+    ctx.fillRect(x, y, border, border * 2);
+    ctx.fillRect(x + width - border * 2, y, border * 2, border);
+    ctx.fillRect(x + width - border, y, border, border * 2);
+    ctx.fillRect(x, y + height - border, border * 2, border);
+    ctx.fillRect(x, y + height - border * 2, border, border * 2);
+    ctx.fillRect(x + width - border * 2, y + height - border, border * 2, border);
+    ctx.fillRect(x + width - border, y + height - border * 2, border, border * 2);
+  }
+
+  // draw border selector
+  function fillBorder(x, y, width, height) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(x, y, width, border);
+    ctx.fillRect(x, y, border, width);
+    ctx.fillRect(x + width - border, y, border, height);
+    ctx.fillRect(x, y + height - border, width, border);
+  }
+
   // get canvas context on start
   useEffect(() => {
     canvas = canvasRef.current;
