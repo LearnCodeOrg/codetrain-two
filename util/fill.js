7209a34132c505928199cba7661b316657b0145f
Cooper Saye <coopersaye@gmail.com>
2022-02-01 23:31:01 -0800

    Extract border fill function.

--- util/fill.js
+++ util/fill.js
@@ -1,6 +1,8 @@
+const fillColor = '#000';
+
 // fills hover marker with given dimensions
 export function fillHover(ctx, border, x, y, width, height) {
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = fillColor;
   ctx.fillRect(x, y, border * 2, border);
   ctx.fillRect(x, y, border, border * 2);
   ctx.fillRect(x + width - border * 2, y, border * 2, border);
@@ -9,4 +11,13 @@ export function fillHover(ctx, border, x, y, width, height) {
   ctx.fillRect(x, y + height - border * 2, border, border * 2);
   ctx.fillRect(x + width - border * 2, y + height - border, border * 2, border);
   ctx.fillRect(x + width - border, y + height - border * 2, border, border * 2);
+}
+
+// fills border marker with given dimensions
+function fillBorder(ctx, border, x, y, width, height) {
+  ctx.fillStyle = fillColor;
+  ctx.fillRect(x, y, width, border);
+  ctx.fillRect(x, y, border, width);
+  ctx.fillRect(x + width - border, y, border, height);
+  ctx.fillRect(x, y + height - border, width, border);
 }
