d90d75f44266b8f958a7090549afe57daf1feb24
Cooper Saye <coopersaye@gmail.com>
2022-02-01 23:25:20 -0800

    Create hover fill function.

--- components/Draw.js
+++ components/Draw.js
@@ -79,6 +79,19 @@ export default function Draw(props) {
     setHoverIndex(-1);
   }
 
+  // draw hover
+  function fillHover(x, y, width, height) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(x, y, border * 2, border);
+    ctx.fillRect(x, y, border, border * 2);
+    ctx.fillRect(x + width - border * 2, y, border * 2, border);
+    ctx.fillRect(x + width - border, y, border, border * 2);
+    ctx.fillRect(x, y + height - border, border * 2, border);
+    ctx.fillRect(x, y + height - border * 2, border, border * 2);
+    ctx.fillRect(x + width - border * 2, y + height - border, border * 2, border);
+    ctx.fillRect(x + width - border, y + height - border * 2, border, border * 2);
+  }
+
   // draws canvas
   function draw() {
     ctx.clearRect(0, 0, spritePx, spritePx);
