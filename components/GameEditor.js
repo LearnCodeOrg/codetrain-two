532ef368305ac2c2d93f42f53fedd05dd8270d21
Cooper Saye <coopersaye@gmail.com>
2022-02-02 20:40:38 -0800

    Initialize editor canvas.

--- components/GameEditor.js
+++ components/GameEditor.js
@@ -1,8 +1,23 @@
+import { useEffect, useRef } from 'react';
+
 import styles from '../styles/components/GameEditor.module.css';
 
+let canvas, ctx;
+
 export default function GameEditor() {
+  const canvasRef = useRef();
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+      />
     </div>
   );
 }
