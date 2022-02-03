f825617cc89492a09fd599b6ffd152cf7887a27a
Cooper Saye <coopersaye@gmail.com>
2022-02-02 22:46:35 -0800

    Handle editor sketching state.

--- components/GameEditor.js
+++ components/GameEditor.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from '../styles/components/GameEditor.module.css';
 
@@ -7,24 +7,32 @@ let canvas, ctx;
 export default function GameEditor() {
   const canvasRef = useRef();
 
-  // called on mouse down
-  function mouseDown() {
+  const [sketching, setSketching] = useState(false);
 
+  // sketches canvas
+  function sketch(e) {
+
+  }
+
+  // called on mouse down
+  function mouseDown(e) {
+    setSketching(true);
+    sketch(e);
   }
 
   // called on mouse move
-  function mouseMove() {
-
+  function mouseMove(e) {
+    if (sketching) sketch(e);
   }
 
   // called on mouse up
-  function mouseUp() {
-
+  function mouseUp(e) {
+    setSketching(false);
   }
 
   // called on mouse leave
-  function mouseLeave() {
-
+  function mouseLeave(e) {
+    setSketching(false);
   }
 
   // get canvas context on start
