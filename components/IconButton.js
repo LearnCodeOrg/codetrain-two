fba1a1585690e7eb1b59e81fb5d85d54d0452ff2
Cooper Saye <coopersaye@gmail.com>
2022-02-04 21:38:47 -0800

    Improve icon button styling.

--- components/IconButton.js
+++ components/IconButton.js
@@ -4,7 +4,10 @@ export default function IconButton(props) {
   const { onClick, icon, down } = props;
 
   return (
-    <button onClick={onClick}>
+    <button
+      className={down ? `${styles.button} ${styles.down}` : styles.button}
+      onClick={onClick}
+    >
       <img src={`/icons/${icon}.png`} />
     </button>
   );
