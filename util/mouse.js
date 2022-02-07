1e34192a8eb26a9e47780f280aff2a6b9f934843
Cooper Saye <coopersaye@gmail.com>
2022-02-07 08:52:02 -0800

    Restructure mouse functions.

--- util/mouse.js
+++ util/mouse.js
@@ -1,16 +1,19 @@
 // returns mouse position rounded to given unit
-export function unitPosition(e, container, unit) {
+export function mousePosition(e, container) {
   // get mouse position
   const mouseX = e.clientX - container.offsetLeft + window.scrollX;
   const mouseY = e.clientY - container.offsetTop + window.scrollY;
-  // get rounded position
-  const unitX = Math.floor(mouseX / unit);
-  const unitY = Math.floor(mouseY / unit);
-  return [unitX, unitY];
+  // return mouse position
+  return [mouseX, mouseY];
 }
 
 // returns index of unit position
-export function unitIndex(e, container, unit, width) {
-  const [unitX, unitY] = unitPosition(e, container, unit);
+export function mouseIndex(e, container, unit, width) {
+  // get mouse position
+  const [mouseX, mouseY] = mousePosition(e, container);
+  // round position
+  const unitX = Math.floor(mouseX / unit);
+  const unitY = Math.floor(mouseY / unit);
+  // return index
   return unitY * width + unitX;
 }
