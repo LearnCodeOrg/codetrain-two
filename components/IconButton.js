96f75fbb02cb6ca40c96807f1dea88365b71b231
Cooper Saye <coopersaye@gmail.com>
2022-02-07 08:03:52 -0800

    Implement auto icon buttons.

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,11 +1,15 @@
 import styles from '../styles/components/IconButton.module.css';
 
 export default function IconButton(props) {
-  const { onClick, icon, down } = props;
+  const { onClick, icon, down, auto } = props;
 
   return (
     <button
-      className={down ? `${styles.button} ${styles.down}` : styles.button}
+      className={
+        auto ? `${styles.button} ${styles.auto}` :
+        down ? `${styles.button} ${styles.down}` :
+        styles.button
+      }
       onClick={onClick}
     >
       <img src={`/icons/${icon}.png`} />
