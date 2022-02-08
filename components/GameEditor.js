98a3c0c19a11922609aae4ddc616826dabc9cd92
Cooper Saye <coopersaye@gmail.com>
2022-02-07 18:08:39 -0800

    Extract functions from GameView.

--- components/GameEditor.js
+++ components/GameEditor.js
@@ -1,6 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+
 import styles from '../styles/components/GameEditor.module.css';
 
-export default function GameEditor() {
+let canvas, ctx;
+
+export default function GameEditor(props) {
+  const { mapPixels } = props;
+
+  const [sketching, setSketching] = useState(false);
+
+  const canvasRef = useRef();
+
+  // sketches canvas
+  function sketch(e) {
+
+  }
+
+  // called on mouse down
+  function mouseDown(e) {
+    setSketching(true);
+    sketch(e);
+  }
+
+  // called on mouse move
+  function mouseMove(e) {
+    if (sketching) sketch(e);
+  }
+
+  // called on mouse up
+  function mouseUp(e) {
+    setSketching(false);
+  }
+
+  // called on mouse leave
+  function mouseLeave(e) {
+    setSketching(false);
+  }
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <canvas
       className={styles.frame}
