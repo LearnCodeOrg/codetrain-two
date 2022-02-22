d9f9bf787be8953e112038a61693687363ef730b
Cooper Saye <coopersaye@gmail.com>
2022-02-21 23:38:00 -0800

    Retrieve user projects.

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,20 @@
 import Header from '../components/Header';
 
 import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, query, where } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
 import styles from '../styles/pages/Projects.module.css';
 
 export default function Projects() {
   const auth = getAuth();
+  const db = getFirestore();
 
   function ProjectsListener() {
+    const uid = auth.currentUser.uid;
+    const projectsRef = collection(db, 'projects-two');
+    const projectsQuery = query(projectsRef, where('uid', '==', uid));
+    const [projects] = useCollectionData(projectsQuery, { idField: 'id' });
   }
 
   return (
