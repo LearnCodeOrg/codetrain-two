2c4a5567df747cc619be69ce84916f723ae06753
Cooper Saye <coopersaye@gmail.com>
2022-02-21 23:37:42 -0800

    Listen for user auth.

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,22 @@
 import Header from '../components/Header';
 
+import { getAuth } from 'firebase/auth';
 import styles from '../styles/pages/Projects.module.css';
 
 export default function Projects() {
+  const auth = getAuth();
+
+  function ProjectsListener() {
+  }
+
   return (
     <div>
       <Header />
+      {
+        auth.currentUser ?
+        <ProjectsListener /> :
+        <p>Sign in to view projects</p>
+      }
     </div>
   );
 }
