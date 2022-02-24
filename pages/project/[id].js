df850812bbfaf3f1a968e7f77ab9d4ea78930190
Cooper Saye <coopersaye@gmail.com>
2022-02-23 22:10:10 -0800

    Get project ID on start.

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,6 +1,17 @@
+import { useRouter } from 'next/router';
 import styles from '../../styles/pages/Project.module.css';
 
 export default function Project() {
+
+  // get project id
+  const router = useRouter();
+  const { id } = router.query;
+
+  // get project data on start
+  useEffect(() => {
+    if (id) getProjectData();
+  }, [id]);
+
   return (
     <div>
     </div>
