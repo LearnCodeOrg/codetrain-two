66454a3b6734d2f1edc496ab1773ccbd6b6645a2
Cooper Saye <coopersaye@gmail.com>
2022-02-23 22:10:34 -0800

    Get project data on start.

--- pages/project/[id].js
+++ pages/project/[id].js
@@ -1,11 +1,24 @@
 import { useRouter } from 'next/router';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+
 import styles from '../../styles/pages/Project.module.css';
 
 export default function Project() {
+  const [data, setData] = useState(undefined);
+
+  const db = getFirestore();
 
   // get project id
   const router = useRouter();
   const { id } = router.query;
+
+  // gets project data from firebase
+  async function getProjectData() {
+    setData(undefined);
+    const projectRef = doc(db, 'projects-two', id);
+    const projectDoc = await getDoc(projectRef);
+    setData(projectDoc.exists ? projectDoc.data() : null);
+  }
 
   // get project data on start
   useEffect(() => {
