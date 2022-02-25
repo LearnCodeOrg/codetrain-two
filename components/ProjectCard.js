92b7533dc6eaa55ef4344fa8395c58546a3d4096
Cooper Saye <coopersaye@gmail.com>
2022-02-25 12:52:40 -0800

    Initialize project card canvas.

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,8 +1,27 @@
+import { useEffect, useRef } from 'react';
+
 import styles from '../styles/components/ProjectCard.module.css';
 
+let canvas, ctx;
+
+const mapPixels = 128;
+
 export default function ProjectCard() {
+  const canvasRef = useRef();
+
+  // get canvas ref on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
+
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+        width={mapPixels}
+        height={mapPixels}
+      />
     </div>
   );
 }
