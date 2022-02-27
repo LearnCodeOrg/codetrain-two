4817dc43abdac91ecffac3dd65a7e120f65e9f10
Cooper Saye <coopersaye@gmail.com>
2022-02-26 19:52:16 -0800

    Initialize tile canvas.

--- components/Tiles.js
+++ components/Tiles.js
@@ -1,8 +1,27 @@
+import { useEffect, useState, useRef } from 'react';
 import styles from '../styles/components/Tiles.module.css';
 
-export default function Tiles() {
+let canvas;
+let ctx;
+
+const canvasWidth = gridWidth * spritePixels;
+const canvasHeight = gridHeight * spritePixels;
+
+export default function Tiles(props) {
+  const canvasRef = useRef();
+
+  // get canvas context on start
+  useEffect(() => {
+    canvas = canvasRef.current;
+    ctx = canvas.getContext('2d');
+  }, []);
   return (
     <div>
+      <canvas
+        ref={canvasRef}
+        width={canvasWidth}
+        height={canvasHeight}
+      />
     </div>
   );
 }
