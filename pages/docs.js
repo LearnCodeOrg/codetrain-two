bc2a2d49e6ea2ca4c7efd676d5c57f99c7de7e9c
Cooper Saye <coopersaye@gmail.com>
2022-02-28 21:02:51 -0800

    Create game loop section in docs.

--- pages/docs.js
+++ pages/docs.js
@@ -7,6 +7,13 @@ export default function Docs() {
     <div>
       <Header />
       <div className={styles.content}>
+        <h1>Docs</h1>
+        <p>These docs provide an overview of the Codetrain engine functionality.</p>
+        <h2>Game Loop</h2>
+        <p>The GameObject class has two built-in functions, start() and update(). The constructor() function can also be used.</p>
+        <p>constructor() runs before the object is constructed.</p>
+        <p>start() runs after all objects are constructed.</p>
+        <p>update() runs once a frame after all objects are constructed.</p>
       </div>
     </div>
   );
