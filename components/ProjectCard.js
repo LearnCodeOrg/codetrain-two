16591bf7c574f3a11a46d23130035e9b72738835
Cooper Saye <coopersaye@gmail.com>
2022-03-02 14:44:05 -0800

    Parse project data.

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import { spriteSquares, mapSprites, mapSquares } from '../util/units';
 import { useEffect, useRef } from 'react';
 
 import styles from '../styles/components/ProjectCard.module.css';
@@ -7,8 +8,19 @@ import styles from '../styles/components/ProjectCard.module.css';
 let canvas, ctx;
 
 const mapPixels = 128;
+const squarePixels = Math.round(mapPixels / mapSquares);
+const spritePixels = Math.round(mapPixels / mapSprites);
 
-export default function ProjectCard() {
+export default function ProjectCard(props) {
+  const { project } = props;
+
+  // parse values from saved data
+  const colors = JSON.parse(project.colors);
+  const tiles = JSON.parse(project.tiles);
+  const objects = JSON.parse(project.objects);
+  const gameTiles = JSON.parse(project.gameTiles);
+  const gameObjects = JSON.parse(project.gameObjects);
+
   const canvasRef = useRef();
 
   // get canvas ref on start
