7c1e78e45c017c16f5697bc83d47f8abf97bd408
Cooper Saye <coopersaye@gmail.com>
2022-03-07 22:44:53 -0800

    Extract MainAuthed component.

--- components/MainAuthed.js
+++ components/MainAuthed.js
@@ -1,8 +1,26 @@
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc } from 'firebase/firestore';
+import { useDocument } from 'react-firebase-hooks/firestore';
+
 import styles from '../styles/components/MainAuthed.module.css';
 
-export default function MainAuthed() {
+export default function MainAuthed(props) {
+  const { Component, pageProps } = props;
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  const uid = auth.currentUser.uid;
+  const userRef = doc(db, 'users', uid);
+  const [userDoc] = useDocument(userRef);
+
+  const currUser = !userDoc ? undefined :
+  !userDoc.exists ? null : userDoc.data();
+
   return (
-    <div>
-    </div>
+    <Component
+      currUser={currUser}
+      {...pageProps}
+    />
   );
 }
