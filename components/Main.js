658f3bb1ca00d536a102068c9bd9a05f1c462085
Cooper Saye <coopersaye@gmail.com>
2022-03-07 22:44:59 -0800

    Clean up main.

--- components/Main.js
+++ components/Main.js
@@ -1,30 +1,9 @@
+import MainAuthed from './MainAuthed';
+
 import { useEffect, useState } from 'react';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, doc } from 'firebase/firestore';
-import { useDocument } from 'react-firebase-hooks/firestore';
 
 import styles from '../styles/components/Main.module.css';
-
-function MainAuthed(props) {
-  const { Component, pageProps } = props;
-
-  const auth = getAuth();
-  const db = getFirestore();
-
-  const uid = auth.currentUser.uid;
-  const userRef = doc(db, 'users', uid);
-  const [userDoc] = useDocument(userRef);
-
-  const currUser = !userDoc ? undefined :
-  !userDoc.exists ? null : userDoc.data();
-
-  return (
-    <Component
-      currUser={currUser}
-      {...pageProps}
-    />
-  );
-}
 
 export default function Main(props) {
   const { Component, pageProps } = props;
@@ -43,7 +22,7 @@ export default function Main(props) {
 
   return (
     authed ?
-    <MainAuthed {...pageProps} /> :
+    <MainAuthed {...props} /> :
     <Component
       currUser={authed}
       {...pageProps}
