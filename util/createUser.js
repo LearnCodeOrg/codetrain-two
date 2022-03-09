12b310ec29cb35eb4a9422e91a177790bffd8ddf
Cooper Saye <coopersaye@gmail.com>
2022-03-08 22:43:18 -0800

    Verify username availability on setup.

--- util/createUser.js
+++ util/createUser.js
@@ -1,5 +1,7 @@
+import { getAuth } from 'firebase/auth';
 // attempts to create user with given username
 export default async function createUser(username) {
+  const auth = getAuth();
   // verify username
   if (!username) {
     alert("Please enter a username.");
@@ -13,6 +15,15 @@ export default async function createUser(username) {
   // verify username length
   if (username.length < 2 || username.length > 16) {
     alert("Username must be between 2 and 16 characters.");
+    return;
+  }
+  // verify username availability
+  const usersRef = collection(db, 'users');
+  const usernameLower = username.toLowerCase();
+  const userQuery = query(usersRef, where('usernameLower', '==', usernameLower));
+  const usernameQuery = await getDocs(userQuery);
+  if (usernameQuery.docs.length) {
+    alert("Username is taken. Please try another.");
     return;
   }
 }
