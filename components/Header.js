3103f2f50ac1a887ecd8b4422172781ccd9669c2
Cooper Saye <coopersaye@gmail.com>
2022-03-08 22:36:36 -0800

    Add tooltips to header buttons.

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import Logo from './Logo';
 import Link from 'next/link';
+import Tooltip from '@mui/material/Tooltip';
 import HelpIcon from '@mui/icons-material/Help';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -33,18 +34,24 @@ export default function Header(props) {
       </Link>
       {
         currUser === null &&
-        <button onClick={setupUser}>
-          <HelpIcon fontSize="large" />
-        </button>
+        <Tooltip title="Setup" arrow disableInteractive>
+          <button onClick={setupUser}>
+            <HelpIcon fontSize="large" />
+          </button>
+        </Tooltip>
       }
       {
         (currUser || currUser === null) ?
-        <button onClick={() => signOut(auth)}>
-          <ArrowCircleRightIcon fontSize="large" />
-        </button> :
-        <button onClick={() => signIn(setupUser)}>
-          <AccountCircleIcon fontSize="large" />
-        </button>
+        <Tooltip title="Sign Out" arrow disableInteractive>
+          <button onClick={() => signOut(auth)}>
+            <ArrowCircleRightIcon fontSize="large" />
+          </button>
+        </Tooltip> :
+        <Tooltip title="Sign In" arrow disableInteractive>
+          <button onClick={() => signIn(setupUser)}>
+            <AccountCircleIcon fontSize="large" />
+          </button>
+        </Tooltip>
       }
     </div>
   );
