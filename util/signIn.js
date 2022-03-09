796a7b17b51686617660a6e913cb8a8f0b90e79b
Cooper Saye <coopersaye@gmail.com>
2022-03-08 22:40:32 -0800

    Check user doc on sign in.

--- util/signIn.js
+++ util/signIn.js
@@ -1,8 +1,17 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 // opens google sign in popup
-export default async function signIn() {
+export default async function signIn(setupUser) {
+  // sign user in
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
   await signInWithPopup(auth, provider);
+  // get user doc
+  const db = getFirestore();
+  const uid = auth.currentUser.uid;
+  const userRef = doc(db, 'users', uid);
+  const userDoc = await getDoc(userRef);
+  // if no user doc, trigger setup
+  if (!userDoc.exists()) setupUser();
 }
