6efd4d1bec4542b4e8859cd64a84a996388a6049
Cooper Saye <coopersaye@gmail.com>
2022-03-09 21:20:14 -0800

    Create sandbox Code component.

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -1,8 +1,18 @@
+import Code from '../components/Code';
+
+import { useState } from 'react';
+
 import styles from '../styles/pages/Sandbox.module.css';
 
 export default function Sandbox() {
+  const [code, setCode] = useState('');
+
   return (
     <div>
+      <Code
+        value={code}
+        onChange={val => setCode(val)}
+      />
     </div>
   );
 }
