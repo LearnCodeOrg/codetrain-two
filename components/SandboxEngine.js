e5ee1cced2e60798b6a5934cf8a0bcc785ca36c0
Cooper Saye <coopersaye@gmail.com>
2022-03-12 11:47:16 -0800

    Extract sandbox engine content.

--- components/SandboxEngine.js
+++ components/SandboxEngine.js
@@ -3,6 +3,90 @@ import styles from '../styles/components/SandboxEngine.module.css';
 export default function SandboxEngine() {
   return (
     <div>
+      <Header {...props} />
+      <Dialog open={saving} onClose={() => setSaving(false)}>
+        <DialogContent>
+          <form
+            className={styles.saveform}
+            onSubmit={e => {
+              e.preventDefault();
+              saveSnippet();
+            }}
+          >
+            Save Snippet
+            <input
+              className="textinput"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Title"
+              required
+            />
+            <button className="textbutton">
+              Save
+            </button>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <div className={
+        docsHidden ? `${styles.docs} ${styles.hidden}` : styles.docs
+      }>
+        <div className={styles.button}>
+          {
+            docsHidden ?
+            <MatButton
+              onClick={() => setDocsHidden(false)}
+              Icon={ArrowCircleRightIcon}
+            /> :
+            <MatButton
+              onClick={() => setDocsHidden(true)}
+              Icon={ArrowCircleLeftIcon}
+            />
+          }
+        </div>
+        <div className={styles.docscontent}>
+          <h2>Docs</h2>
+          <p>log(text): logs given text</p>
+          <p>logImage(url): logs given image</p>
+          <p>prompt(text): opens prompt and returns input</p>
+          <p>alert(text): opens alert</p>
+        </div>
+      </div>
+      <div>
+        <Code
+          value={code}
+          onChange={val => setCode(val)}
+        />
+        <div className={styles.toolbar}>
+          <IconButton
+            onClick={compile}
+            icon="play"
+          />
+          <IconButton
+            onClick={save}
+            icon="save"
+          />
+        </div>
+      </div>
+      <div>
+        <p>Console</p>
+        <button onClick={clear}>
+          Clear
+        </button>
+          {
+            logs.map((log, i) =>
+              <div key={i}>
+                {
+                  log.type === 'text' ?
+                  <p>{log.text}</p> :
+                  log.type === 'image' ?
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img src={log.url} alt={log.url} /> :
+                  null
+                }
+              </div>
+            )
+          }
+      </div>
     </div>
   );
 }
