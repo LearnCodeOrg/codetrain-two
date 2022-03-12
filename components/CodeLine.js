2d259805220e7157b8aeb5915c9cbfb70ae470e5
Cooper Saye <coopersaye@gmail.com>
2022-03-12 15:37:47 -0800

    Extract CodeLine component.

--- components/CodeLine.js
+++ components/CodeLine.js
@@ -1,8 +1,19 @@
 import styles from '../styles/components/CodeLine.module.css';
 
-export default function CodeLine() {
+export default function CodeLine(props) {
   return (
-    <div>
-    </div>
+    <p className={styles.line}>
+      {
+        props.children.split('`').map((text, i) =>
+          <span key={i}>
+            {
+              i % 2 ?
+              <code className="language-javascript">{text}</code> :
+              <>{text}</>
+            }
+          </span>
+        )
+      }
+    </p>
   );
 }
