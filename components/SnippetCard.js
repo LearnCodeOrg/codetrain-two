b44ea59f47f7b489e404757cfde2ec14f74e6c9d
Cooper Saye <coopersaye@gmail.com>
2022-03-12 10:29:50 -0800

    Add link to snippet in card.

--- components/SnippetCard.js
+++ components/SnippetCard.js
@@ -1,8 +1,17 @@
+import Link from 'next/link';
+
 import styles from '../styles/components/SnippetCard.module.css';
 
-export default function SnippetCard() {
+export default function SnippetCard(props) {
+  const { snippet } = props;
+
   return (
     <div>
+      <Link href={`/snippet/${snippet.id}`}>
+        <a>
+          <p>{snippet.title}</p>
+        </a>
+      </Link>
     </div>
   );
 }
