13f7bdaf9d13a96aa999e0ed398106fe029c96f3
Cooper Saye <coopersaye@gmail.com>
2022-03-11 17:54:27 -0800

    Add tooltip and icon to MatButton.

--- components/MatButton.js
+++ components/MatButton.js
@@ -1,8 +1,15 @@
+import Tooltip from './Tooltip';
+
 import styles from '../styles/components/MatButton.module.css';
 
-export default function MatButton() {
+export default function MatButton(props) {
+  const { Icon, tooltip, onClick } = props;
+
   return (
-    <div>
-    </div>
+    <Tooltip title={tooltip}>
+      <button onClick={onClick}>
+        <Icon />
+      </button>
+    </Tooltip>
   );
 }
