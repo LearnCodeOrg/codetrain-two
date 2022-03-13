dbb2883e57a24f519f42809493b966fd15e59073
Cooper Saye <coopersaye@gmail.com>
2022-03-13 10:15:56 -0700

    Map snippets to options.

--- components/SelectSnippet.js
+++ components/SelectSnippet.js
@@ -1,7 +1,11 @@
-import styles from '../styles/components/SelectSnippet.module.css';
 import { getFirestore, collection, query, where } from 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import styles from '../styles/components/SelectSnippet.module.css';
+
 export default function SelectSnippet(props) {
+  const { currUser, currSnippet, loadSnippet } = props;
+
   const db = getFirestore();
 
   const snippetsRef = collection(db, 'snippets');
@@ -28,6 +32,15 @@ export default function SelectSnippet(props) {
         }
       }}
     >
+      <option value=""></option>
+      {
+        snippets &&
+        snippets.map(snippet =>
+          <option key={snippet.id} value={snippet.id}>
+            {snippet.title}
+          </option>
+        )
+      }
     </select>
   );
 }
