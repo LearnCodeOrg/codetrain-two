7c5b917dbadf6f927a9dfe15ac43d8db049bdb56
Cooper Saye <coopersaye@gmail.com>
2022-03-13 10:15:11 -0700

    Set up snippet listener.

--- components/SelectSnippet.js
+++ components/SelectSnippet.js
@@ -1,6 +1,21 @@
 import styles from '../styles/components/SelectSnippet.module.css';
+import { getFirestore, collection, query, where } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+export default function SelectSnippet(props) {
+  const db = getFirestore();
 
-export default function SelectSnippet() {
+  const snippetsRef = collection(db, 'snippets');
+  const snippetsQuery = query(snippetsRef, where('uid', '==', currUser.id));
+  const [snippets] = useCollectionData(snippetsQuery, { idField: 'id' });
+
+  // return if loading snippets
+  if (!snippets) return <p>Loading...</p>;
+  if (!snippets.length) return (
+    <>
+      <p>No snippets yet</p>
+    </>
+  );
+
   return (
     <div>
     </div>
