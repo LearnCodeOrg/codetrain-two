5fb9ed625702e7aeca80575a751fd171c2c0566e
Cooper Saye <coopersaye@gmail.com>
2022-03-13 10:28:25 -0700

    Set up snippet listener.

--- pages/snippets.js
+++ pages/snippets.js
@@ -1,6 +1,24 @@
 import styles from '../styles/pages/Snippets.module.css';
 
 export default function Snippets() {
+  function SnippetsListener() {
+    const uid = auth.currentUser.uid;
+    const snippetsRef = collection(db, 'snippets');
+    const snippetsQuery = query(snippetsRef, where('uid', '==', uid));
+    const [snippets] = useCollectionData(snippetsQuery, { idField: 'id' });
+
+    // return if loading snippets
+    if (!snippets) return <p>Loading...</p>;
+    if (!snippets.length) return (
+      <>
+        <p>No snippets yet</p>
+        <Link href="/create">
+          <a>Create a snippet</a>
+        </Link>
+      </>
+    );
+  }
+
   return (
     <div>
     </div>
