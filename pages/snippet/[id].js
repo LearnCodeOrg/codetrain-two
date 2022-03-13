6cc890a84f1068dc7084ad3f05fab9d179d78fa3
Cooper Saye <coopersaye@gmail.com>
2022-03-13 10:17:21 -0700

    Find snippet on start.

--- pages/snippet/[id].js
+++ pages/snippet/[id].js
@@ -1,6 +1,19 @@
-import styles from '../styles/pages/Snippet.module.css';
+import styles from '../../styles/pages/Snippet.module.css';
 
-export default function Snippet() {
+export default function Snippet(props) {
+  // gets snippet data from firebase
+  async function getSnippetData() {
+    setData(undefined);
+    const snippetRef = doc(db, 'snippets', id);
+    const snippetDoc = await getDoc(snippetRef);
+    setData(snippetDoc.exists ? { id, ...snippetDoc.data() } : null);
+  }
+
+  // get project data on start
+  useEffect(() => {
+    if (id) getSnippetData();
+  }, [id]);
+
   return (
     <div>
     </div>
