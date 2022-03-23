6abcdfe81c308b229702c6dba5a250084a6feead
Cooper Saye <coopersaye@gmail.com>
2022-03-23 09:29:56 -0700

    Create theme toggle button.

--- components/CodeEditor.js
+++ components/CodeEditor.js
@@ -1,18 +1,26 @@
 import AceEditor from 'react-ace';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
+import MatButton from '../components/MatButton';
+
+import { useState } from 'react';
 
 import 'ace-builds/src-noconflict/mode-javascript.js';
 import 'ace-builds/src-noconflict/theme-monokai.js';
+import 'ace-builds/src-noconflict/theme-tomorrow.js';
 import 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/ext-searchbox';
 
 import styles from '../styles/components/CodeEditor.module.css';
 
 export default function CodeEditor(props) {
+  const [mode, setMode] = useState('dark');
+
   return (
     <div className={styles.container}>
     <AceEditor
       mode="javascript"
-      theme="monokai"
+      theme={mode === 'dark' ? 'monokai' : 'tomorrow'}
       wrapEnabled={true}
       showPrintMargin={false}
       tabSize={2}
@@ -35,6 +43,14 @@ export default function CodeEditor(props) {
         []
       }
       {...props}
+    />
+    <MatButton
+      onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+      Icon={
+        mode === 'dark' ?
+        ModeNightIcon :
+        LightModeIcon
+      }
     />
     </div>
   );
