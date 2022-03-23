944c4c164f490d02dbbf54ad2c0b45da8fa6f2fb
Cooper Saye <coopersaye@gmail.com>
2022-03-23 09:17:03 -0700

    Pass parent class into MatButton.

--- components/MatButton.js
+++ components/MatButton.js
@@ -3,21 +3,21 @@ import Tooltip from './Tooltip';
 import styles from '../styles/components/MatButton.module.css';
 
 export default function MatButton(props) {
-  const { Icon, tooltip, onClick } = props;
+  const { Icon, tooltip, onClick, className } = props;
 
   return (
     tooltip ?
     <Tooltip title={tooltip}>
       <button
         onClick={onClick}
-        className={styles.button}
+        className={`${className} ${styles.button}`}
       >
         <Icon fontSize="large" />
       </button>
     </Tooltip> :
     <button
       onClick={onClick}
-      className={styles.button}
+      className={`${className} ${styles.button}`}
     >
       <Icon fontSize="large" />
     </button>
